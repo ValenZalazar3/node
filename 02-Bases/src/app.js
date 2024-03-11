98583d140b55fdbc747049287ce4dd6f3e9a7ce8
Valen <valenzalazar3@gmail.com>
2024-03-11 12:36:53 -0300

    Bases

--- 02-Bases/src/app.js
+++ 02-Bases/src/app.js
@@ -1,3 +1,13 @@
-const { emailTemplate } = require('./js-foundation/01-template.js')
+// const { emailTemplate } = require('./js-foundation/01-template.js')
+// require('./js-foundation/02-destructuring')
+// const { getUserById } = require('./js-foundation/03-callbacks.js')
+const { getUserById } = require('./js-foundation/04-arrow')
 
-console.log(emailTemplate)
+// console.log(emailTemplate)
+const id = 1
+getUserById(id, (error, user) => {
+  if (error) {
+    throw new Error(error)
+  }
+  console.log(user)
+})
