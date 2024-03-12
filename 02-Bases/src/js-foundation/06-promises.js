f0c446050d220756eaa35d900b4c625dd6bf007c
Valen <valenzalazar3@gmail.com>
2024-03-12 11:24:56 -0300

    Bases final 02

--- 02-Bases/src/js-foundation/06-promises.js
+++ 02-Bases/src/js-foundation/06-promises.js
@@ -1,11 +1,21 @@
-const getPokemonById = (id) => {
+const { http } = require('../plugins')
+
+const getPokemonById = async (id) => {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`
 
-  fetch(url).then((response) => {
-    response.json().then((pokemon) => {
-      console.log(pokemon.name)
-    })
-  })
+  const pokemon = await http.get(url)
+
+  // const resp = await fetch( url );
+  // const pokemon = await resp.json();
+
+  // throw new Error('Pokemon no existe');
+
+  return pokemon.name
+
+  // return fetch( url )
+  //   .then( ( resp ) => resp.json())
+  // .then( () => { throw new Error('Pokemon no existe') })
+  //   .then( ( pokemon ) => pokemon.name );
 }
 
 module.exports = getPokemonById
--- 02-Bases/src/plugins/index.js
+++ 02-Bases/src/plugins/index.js
@@ -1,7 +1,10 @@
-const { getId } = require('./get-id.plugin')
-const { getAge } = require('./get-age.plugin')
+const { getUUID } = require('../plugins/get-id.plugin')
+const { getAge } = require('../plugins/get-age.plugin')
+
+const { http } = require('../plugins/http-client.plugin')
 
 module.exports = {
-  getId,
-  getAge
+  getAge,
+  getUUID,
+  http
 }
