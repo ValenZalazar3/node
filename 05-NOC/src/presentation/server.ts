e1cdae11b41cf7a72995806edc4e6c11f12de81f
Valen <valenzalazar3@gmail.com>
2024-04-01 17:05:34 -0300

    Comienzo seccion 8

---
+++ 05-NOC/src/presentation/server.ts
@@ -0,0 +1,19 @@
+import { CheckService } from "../domain/use-cases/checks/check-service";
+import { CronService } from "./cron/cron-service";
+
+
+
+
+
+export class Server {
+
+    public static start() {
+        console.log('Server started...')
+        CronService.createJob(
+            '*/5 * * * * *',
+            () => {
+                new CheckService().execute('http://google.com')
+            }
+        )
+    }
+}
