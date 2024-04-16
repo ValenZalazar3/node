256998d5a50dca07ed0788a9032649f264a11b24
Valen <valenzalazar3@gmail.com>
2024-04-16 13:29:56 -0300

    fin seccion 14

---
+++ 07-RESTWeb/src/presentation/routes.ts
@@ -0,0 +1,32 @@
+import { Router } from "express";
+
+import { TodoRoutes } from "./todos/routes";
+
+
+
+
+export class AppRoutes {
+
+    static get routes(): Router {
+
+        const router = Router()
+
+
+
+
+
+        router.use('/api/todo', TodoRoutes.routes)
+
+
+        return router
+
+    }
+
+
+
+
+
+
+}
+
+
