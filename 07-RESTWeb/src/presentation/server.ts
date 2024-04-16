da308e74ece5adfbe42d1e58f9cee4c20354a661
Valen <valenzalazar3@gmail.com>
2024-04-16 13:29:58 -0300

    fin seccion 14

--- 07-RESTWeb/src/presentation/server.ts
+++ 07-RESTWeb/src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from 'express'
+import express, { Router } from 'express'
 import path from 'path'
 
 interface Options {
-    port: number,
+    port: number;
+    routes: Router;
     public_path?: string
 }
 
@@ -10,25 +11,32 @@ export class Server {
     private app = express()
     private readonly port: number
     private readonly public_path: string
+    private readonly routes: Router;
 
 
 
 
     constructor(options: Options) {
-        const { port, public_path = 'public' } = options
+        const { port, routes, public_path = 'public' } = options
         this.port = port;
         this.public_path = public_path
+        this.routes = routes
     }
 
 
 
     async start() {
-        //* Middlewares
-
+        //* Middlewares ==> te ayuda para el create.
+        this.app.use(express.json()); // raw 
+        this.app.use(express.urlencoded({ extended: true })) // x-www-form-urlencode
 
         //* Public Folder
         this.app.use(express.static(this.public_path))
 
+        //*Routes
+        this.app.use(this.routes)
+
+        //* SPA (cualquier ruta va a pasar por acas)
         this.app.get('*', (req, res) => {
             const indexPath = path.join(__dirname + `${this.public_path}`);
             res.sendFile(indexPath);
